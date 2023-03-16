89babaeac1b9a4f5328768bf75bbba342708c12e
Oscarrscso <118562321+Oscarrscso@users.noreply.github.com>
2023-03-16 01:10:16 +0000

    refactored code

--- script.js
+++ script.js
@@ -1,252 +1,96 @@
-// Amount of wavv
-var waavVal = 0;
-// Waav per click
+// Game state variables
+let waavVal = 0;
 let waavPC = 1;
-// Waav per second
-let waavPS = 0
+let waavPS = 0;
 
-// unlucky bud
+// Helper functions
+const updateWaavDisplay = () => {
+  document.getElementById("waav").innerHTML = `waav: ${waavVal}`;
+};
 
+const updateUpgradeCost = (elementId, currentCost) => {
+  const newCost = Math.floor(currentCost * 1.5);
+  document.getElementById(elementId).innerHTML = newCost;
+  return newCost;
+};
 
-// Add waav per click/crit
+const animateText = (elementId, property, start, end, duration) => {
+  const element = document.getElementById(elementId);
+  element.style[property] = start;
+  setTimeout(() => {
+    element.style[property] = end;
+  }, duration);
+};
+
+const canAffordUpgrade = (cost) => {
+  if (waavVal >= cost) {
+    waavVal -= cost;
+    updateWaavDisplay();
+    return true;
+  }
+  return false;
+};
+
+const changeButtonColor = (elementId, color, duration) => {
+  const btn = document.getElementById(elementId);
+  btn.style.background = color;
+  setTimeout(() => {
+    btn.style.background = "rgb(31, 31, 31)";
+  }, duration);
+};
+
+// Main functions
 const waavBTN = () => {
-    waavVal += waavPC;
-    document.getElementById("waav").innerHTML = `waav: ${waavVal}`
-    // critical hit chance and reward
-    let critical = Math.floor(Math.random() * 350)
-    if (critical === 1){
-        waavVal += (waavPC*50)
-        let crit = document.getElementById("critical")
-        document.getElementById("critical").innerHTML = `Critical Hit! +${waavPC*50} waav!`
-        crit.classList.add("is-active")
-        let int = setTimeout(count, 2000)
-        function count () 
-{
-        crit.classList.remove("is-active")
-{
-}
-}
-}
-}
+  waavVal += waavPC;
+  updateWaavDisplay();
 
+  const critical = Math.floor(Math.random() * 350);
+  if (critical === 1) {
+    const critReward = waavPC * 50;
+    waavVal += critReward;
+    document.getElementById("critical").innerHTML = `Critical Hit! +${critReward} waav!`;
+    animateText("critical", "opacity", 1, 0, 2000);
+  }
+};
 
+const upgradeClick = (upgradeAmount, costElementId, buttonElementId) => {
+  const cost = parseInt(document.getElementById(costElementId).innerHTML, 10);
+  if (canAffordUpgrade(cost)) {
+    waavPC += upgradeAmount;
+    updateUpgradeCost(costElementId, cost);
+    document.getElementById("clickAmount").innerHTML = `${waavPC} waav per click.`;
+    animateText("clickAmount", "scale", 1.04, 1, 200);
+  } else {
+    changeButtonColor(buttonElementId, 'firebrick', 200);
+  }
+};
 
+const add1 = () => upgradeClick(1, "item1Cost", "add1Click");
+const add10 = () => upgradeClick(10, "item2Cost", "add10Click");
+const add100 = () => upgradeClick(100, "item4Cost", "add100Click");
+const add500 = () => upgradeClick(500, "item6Cost", "add500Click");
 
-
-    
-
-// Add 1 extra waav per click
-const add1 = () => 
-{
-    
-    let cost = document.getElementById("item1Cost").innerHTML;
-    if (waavVal >= cost) 
-{
-        waavVal -= cost;
-        let addVar = 1 //change!!!!!!!
-        waavPC += addVar;
-            document.getElementById("waav").innerHTML = `waav: ${waavVal}`
-            
-            document.getElementById("item1Cost").innerHTML = Math.floor(cost * 1.5);
-            document.getElementById("clickAmount").innerHTML = `${waavPC} waav per click.`
-            document.getElementById("clickAmount").style.scale = 1.04
-            let int = setTimeout(count, 200)
-            function count (){
-                document.getElementById("clickAmount").style.scale = 1
-            }
-} 
-    else
-{
-        let btn = document.getElementById("add1Click")
-            btn.style.background = 'firebrick'
-        let int = setInterval(count, 200)
-            function count () 
-{
-    btn.style.background = "rgb(31, 31, 31)"
-                if (count)
-{
-                clearInterval(int)
-}
-}
-}
-}
-
-// Add 10 extra waav per click
-const add10 = () => 
-{
-    let cost = document.getElementById("item2Cost").innerHTML;
-    if (waavVal >= cost) 
-{
-        waavVal -= cost;
-        waavPC += 10;
-            document.getElementById("waav").innerHTML = `waav: ${waavVal}`
-            document.getElementById("item2Cost").innerHTML = Math.floor(cost * 1.5);
-            document.getElementById("clickAmount").innerHTML = `${waavPC} waav per click.`
-            document.getElementById("clickAmount").style.scale = 1.04
-            let int = setTimeout(count, 200)
-            function count (){
-                document.getElementById("clickAmount").style.scale = 1
-            }
-} 
-    else
-{
-        let btn = document.getElementById("add10Click")
-            btn.style.background = 'firebrick'
-        let int = setInterval(count, 200)
-            function count () 
-{
-    btn.style.background = "rgb(31, 31, 31)"
-                if (count)
-{
-                clearInterval(int)
-}
-}
-}
-}
-
-// Add 100 extra waav per click
-const add100 = () => 
-{
-    let cost = document.getElementById("item4Cost").innerHTML;
-    if (waavVal >= cost) 
-{
-        waavVal -= cost;
-        waavPC += 100;
-            document.getElementById("waav").innerHTML = `waav: ${waavVal}`
-            document.getElementById("item4Cost").innerHTML = Math.floor(cost * 1.5);
-            document.getElementById("clickAmount").innerHTML = `${waavPC} waav per click.`
-            document.getElementById("clickAmount").style.scale = 1.04
-            let int = setTimeout(count, 200)
-            function count (){
-                document.getElementById("clickAmount").style.scale = 1
-            }
-} 
-    else
-{
-        let btn = document.getElementById("add100Click")
-            btn.style.background = 'firebrick'
-        let int = setInterval(count, 200)
-            function count () 
-{
-         btn.style.background = "rgb(31, 31, 31)"
-                if (count)
-{
-                clearInterval(int)
-}
-}
-}
-}
-
-// Add 500 extra waav per click
-const add500 = () => 
-{
-    let cost = document.getElementById("item6Cost").innerHTML;
-    if (waavVal >= cost) 
-{
-        waavVal -= cost;
-        waavPC += 500;
-            document.getElementById("waav").innerHTML = `waav: ${waavVal}`
-            document.getElementById("item6Cost").innerHTML = Math.floor(cost * 1.5);
-            document.getElementById("clickAmount").innerHTML = `${waavPC} waav per click.`
-            document.getElementById("clickAmount").style.scale = 1.04
-            let int = setTimeout(count, 200)
-            function count (){
-                document.getElementById("clickAmount").style.scale = 1
-            }
-} 
-    else
-{
-        let btn = document.getElementById("add500Click")
-            btn.style.background = 'firebrick'
-        let int = setInterval(count, 200)
-            function count () 
-{
-         btn.style.background = "rgb(31, 31, 31)"
-                if (count)
-{
-                clearInterval(int)
-}
-}
-}
-}
-
-// Add 1 waav per second
-const onePS = () =>
-{
-  let cost = document.getElementById("item3Cost").innerHTML 
-        if (waavVal >= cost)
-{
-    waavVal -= cost
-    document.getElementById("clickPS").style.visibility = "visible"
-    document.getElementById("clickPS").style.scale = 1.04
-            let int = setTimeout(count, 200)
-            function count (){
-                document.getElementById("clickPS").style.scale = 1
-            }
-    setInterval(counter, 1000)
-    function counter()
-{
-    waavVal+=1
-        document.getElementById("waav").innerHTML = `waav: ${waavVal}`
-        
-} 
-    waavPS+=1
-    document.getElementById("clickPS").innerHTML =`${waavPS} waav per second.`
-    document.getElementById("item3Cost").innerHTML = Math.floor(cost * 1.5);
-} 
-    else
-{
-        let btn = document.getElementById("onePS")
-            btn.style.background = 'firebrick'
-        let int = setInterval(count, 200)
-            function count () 
-{
-    btn.style.background = "rgb(31, 31, 31)"
-                if (count)
-{
-                clearInterval(int)
-}
-}
-}
-} 
-
-// Add 10 waav per second
-const tenPS = () =>
-{
-  let cost = document.getElementById("item5Cost").innerHTML 
-        if (waavVal >= cost)
-{
-    waavVal -= cost
-    document.getElementById("clickPS").style.visibility = "visible"
-    document.getElementById("clickPS").style.scale = 1.04
-            let int = setTimeout(count, 200)
-            function count (){
-                document.getElementById("clickPS").style.scale = 1
-            }
-    setInterval(counter, 1000)
-    function counter()
-{
-    waavVal+=10
-        document.getElementById("waav").innerHTML = `waav: ${waavVal}`
-        
-} 
-    waavPS+=10
-    document.getElementById("clickPS").innerHTML =`${waavPS} waav per second.`
-    document.getElementById("item5Cost").innerHTML = Math.floor(cost * 1.5);
-} 
-    else
-{
-        let btn = document.getElementById("tenPS")
-            btn.style.background = 'firebrick'
-        let int = setInterval(count, 200)
-            function count () 
-{
-    btn.style.background = "rgb(31, 31, 31)"
-                if (count)
-{
-                clearInterval(int)
-}
-}
-}
-} 
-
+const addWaavPS = (waavToAdd, costElementId, buttonElementId) => {
+    const cost = parseInt(document.getElementById(costElementId).innerHTML, 10);
+    if (canAffordUpgrade(cost)) {
+      waavPS += waavToAdd;
+      document.getElementById("clickPS").style.visibility = "visible";
+      document.getElementById("clickPS").innerHTML = `${waavPS} waav per second.`;
+      animateText("clickPS", "scale", 1.04, 1, 200);
+  
+      // Only set up the interval if it hasn't been set up before
+      if (!window.waavInterval) {
+        window.waavInterval = setInterval(() => {
+          waavVal += waavPS;
+          updateWaavDisplay();
+        }, 1000);
+      }
+  
+      updateUpgradeCost(costElementId, cost);
+    } else {
+      changeButtonColor(buttonElementId, 'firebrick', 200);
+    }
+  };
+  
+  const onePS = () => addWaavPS(1, "item3Cost", "onePS");
+  const tenPS = () => addWaavPS(10, "item5Cost", "tenPS");
